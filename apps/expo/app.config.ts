23ca8b61e044b820ef504e4b046c1a14700b1c65
Xingyu(Alex) Shen <89829751+shenxingy@users.noreply.github.com>
2025-10-23 21:34:52 -0400

    chore: update app configuration with new bundle identifier and project ID

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -2,9 +2,9 @@ import type { ConfigContext, ExpoConfig } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: "expo",
-  slug: "expo",
-  scheme: "expo",
+  name: "MealMates",
+  slug: "mealmates",
+  scheme: "mealmates",
   version: "0.1.0",
   orientation: "portrait",
   icon: "./assets/icon-light.png",
@@ -15,7 +15,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   newArchEnabled: true,
   assetBundlePatterns: ["**/*"],
   ios: {
-    bundleIdentifier: "your.bundle.identifier",
+    bundleIdentifier: "com.mealmates.app",
     supportsTablet: true,
     icon: {
       light: "./assets/icon-light.png",
@@ -23,18 +23,18 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     },
   },
   android: {
-    package: "your.bundle.identifier",
+    package: "com.mealmates.app",
     adaptiveIcon: {
       foregroundImage: "./assets/icon-light.png",
       backgroundColor: "#1F104A",
     },
     edgeToEdgeEnabled: true,
   },
-  // extra: {
-  //   eas: {
-  //     projectId: "your-eas-project-id",
-  //   },
-  // },
+  extra: {
+    eas: {
+      projectId: "8a2987e1-f5b3-4c35-b410-8b0062330a28",
+    },
+  },
   experiments: {
     tsconfigPaths: true,
     typedRoutes: true,
