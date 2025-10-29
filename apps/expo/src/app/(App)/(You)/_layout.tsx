4d91a9d8395725b7ddc97f584d805412079bb84b
chitose <103087652@qq.com>
2025-10-29 19:42:48 -0400

    feat: enhance EventView with username and mood display; update AnimatedPageFrame layout

--- apps/expo/src/app/(App)/(You)/_layout.tsx
+++ apps/expo/src/app/(App)/(You)/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function YouLayout() {
   return (
     <>
-      <Stack screenOptions={{headerShown: false}}>
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" options={{ title: "You Page" }} />
       </Stack>
     </>
