b3010caff3a297e65675b5fb51d4014341e74858
chitose <103087652@qq.com>
2025-10-29 18:54:43 -0400

    feat: update HomePage layout with enhanced user profile details and message display

--- apps/expo/src/app/(App)/(Home)/index.tsx
+++ apps/expo/src/app/(App)/(Home)/index.tsx
@@ -1,12 +1,9 @@
 import { Image, Platform, StyleSheet, Text, View } from "react-native";
 import { GlassView, isLiquidGlassAvailable } from "expo-glass-effect";
 import { SymbolView } from "expo-symbols";
-
-
-
-import AnimatedPageFrame from "../../../../components/frame/AnimatedPageFrame";
 import { Ionicons } from "@expo/vector-icons";
 
+import AnimatedPageFrame from "../../../../components/frame/AnimatedPageFrame";
 
 export default function HomePage() {
   // Gradient color
@@ -36,20 +33,63 @@ export default function HomePage() {
                 style={styles.selfie}
               />
             </GlassView>
-            <Text style={styles.userName}>Toshi Li</Text>
+            <Text style={styles.userNameText}>Toshi Li</Text>
           </View>
           <View style={styles.scheduleTimeContainer}>
-            {Platform.OS === "ios" ?
-              <SymbolView
-                name="clock"
-                style={{ width: 24, height: 24 }}
-                tintColor="#ff7800"
-              /> :
+            {Platform.OS === "ios" ? (
+              <SymbolView name="clock" style={{ width: 24, height: 24 }} />
+            ) : (
               <Ionicons name="time" size={24} color="#ff7800" />
-            }
-            <Text style={styles.scheduleTime}>12:30</Text>
+            )}
+            <Text style={styles.normalText}>12:30</Text>
           </View>
         </View>
+        <View style={styles.detailedInfoContainer}>
+          <View style={styles.detailedInfo}>
+            {Platform.OS === "ios" ? (
+              <SymbolView
+                name="mappin.circle"
+                style={{ width: 24, height: 24 }}
+              />
+            ) : (
+              <Ionicons name="location" size={24} color="#ff7800" />
+            )}
+            <Text style={styles.normalText}>Duke Chapel</Text>
+          </View>
+        </View>
+        <View style={styles.detailedInfoContainer}>
+          <View style={styles.detailedInfo}>
+            {Platform.OS === "ios" ? (
+              <SymbolView
+                name="storefront.circle"
+                style={{ width: 24, height: 24 }}
+              />
+            ) : (
+              <Ionicons name="restaurant" size={24} color="#ff7800" />
+            )}
+            <Text style={styles.normalText}>JB roast</Text>
+          </View>
+        </View>
+        <GlassView
+          style={
+            isLiquidGlassAvailable()
+              ? styles.glassMessageContainer
+              : styles.nonGlassMessageContainer
+          }
+        >
+          {Platform.OS === "ios" ? (
+            <SymbolView
+              name="message.badge.circle"
+              style={{ width: 24, height: 24 }}
+            />
+          ) : (
+            <Ionicons name="chatbubbles" size={24} color="#ff7800" />
+          )}
+          <Text style={{ flex: 1, fontSize: 16, fontWeight: "normal" }}>
+            You're welcome to eat together! I'll be waiting near the entrance of
+            Duke Chapel waiting for you.
+          </Text>
+        </GlassView>
       </GlassView>
       {Array.from({ length: 20 }, (_, i) => (
         <View key={i} style={styles.contentBlock}>
@@ -92,27 +132,33 @@ const styles = StyleSheet.create({
     fontSize: 14,
     lineHeight: 20,
   },
+  normalText: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#2c2c2c",
+    textAlign: "center",
+  },
   glassContainer: {
-    height: 200,
     borderRadius: 30,
     overflow: "hidden",
     flexDirection: "column",
     justifyContent: "flex-start",
+    gap: 10,
   },
   nonGlassContainer: {
-    height: 200,
     borderRadius: 30,
     overflow: "hidden",
     backgroundColor: "rgba(255, 255, 255, 0.8)",
     flexDirection: "column",
     justifyContent: "flex-start",
+    gap: 10,
   },
   infoContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    paddingHorizontal: 15,
-    paddingTop: 15,
+    marginHorizontal: 15,
+    marginTop: 15,
   },
   profileGroup: {
     flexDirection: "row",
@@ -130,21 +176,49 @@ const styles = StyleSheet.create({
     height: "100%",
     borderRadius: 24,
   },
-  userName: {
+  userNameText: {
     fontSize: 20,
     fontWeight: "bold",
     textAlign: "center",
-  },
-  scheduleTime: {
-    fontSize: 16,
-    fontWeight: "600",
-    color: "#ff7800",
-    textAlign: "center",
+    color: "#424242",
   },
   scheduleTimeContainer: {
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
     gap: 5,
+  },
+  detailedInfoContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    gap: 10,
+    marginHorizontal: 30,
+  },
+  detailedInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+  },
+  glassMessageContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    marginHorizontal: 15,
+    borderRadius: 20,
+    marginBottom: 15,
+    gap: 5,
+    padding: 15,
+  },
+  nonGlassMessageContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    marginHorizontal: 15,
+    borderRadius: 20,
+    marginBottom: 15,
+    gap: 5,
+    padding: 15,
+    backgroundColor: "rgba(205,205,205,0.5)",
   },
 });
