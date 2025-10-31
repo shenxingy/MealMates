bc3c57b8733e06b5116bcf47bf4e0d9c6fc3c92d
chitose <103087652@qq.com>
2025-10-30 20:34:46 -0400

    feat: implement API client with error handling; add fetch function for simple event list; update HomePage to display events

--- apps/expo/src/app/(App)/(Home)/index.tsx
+++ apps/expo/src/app/(App)/(Home)/index.tsx
@@ -1,18 +1,42 @@
-import { Pressable, StyleSheet, View } from "react-native";
+import { useQuery } from "@tanstack/react-query";
+import { Pressable, StyleSheet, View, Text } from "react-native";
 
 import AnimatedPageFrame from "../../../../components/frame/AnimatedPageFrame";
 import EventView from "../../../../components/Homepage/EventView";
+import { fetchSimpleEventList } from "~/utils/api";
 
 export default function HomePage() {
   // Gradient color
   const baseColor = "255,120,0";
   const header = "MealMate";
 
+  // useQuery to fetch simple event list
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["simpleEventList"],
+    queryFn: fetchSimpleEventList,
+  });
+
+  if (isLoading) {
+    return (
+      <AnimatedPageFrame baseColor={baseColor} headerTitle={header}>
+        <Text>Loading...</Text>
+      </AnimatedPageFrame>
+    );
+  }
+
+  if (error) {
+    return (
+      <AnimatedPageFrame baseColor={baseColor} headerTitle={header}>
+        <Text>Error loading data</Text>
+      </AnimatedPageFrame>
+    );
+  }
+
   return (
     <AnimatedPageFrame baseColor={baseColor} headerTitle={header}>
       <View style={styles.emptySpace}></View>
 
-      {Array.from({ length: 20 }, (_, i) => (
+      {data?.map((event, i) => (
         <View key={i} style={{ marginBottom: 20 }}>
           <Pressable
             onPress={() => {
@@ -20,14 +44,13 @@ export default function HomePage() {
             }}
           >
             <EventView
-              scheduleTime={`12:${i}`}
-              username={"Nagasaki Soyo"}
-              mood={"🤓"}
-              meetPoint={"Tsuki no mori Girls' School"}
-              restaurantName={"RING"}
-              message={
-                "Hello! Let's have lunch together. Let me know if you're interested. Could be fun!"
-              }
+              scheduleTime={event.scheduleTime}
+              username={event.username}
+              avatarUrl={event.avatarUrl}
+              mood={event.mood}
+              meetPoint={event.meetPoint}
+              restaurantName={event.restaurantName}
+              message={event.message}
             />
           </Pressable>
         </View>
---
+++ apps/expo/src/definition.ts
@@ -0,0 +1,15 @@
+export interface ApiResponse<T> {
+  data: T;
+  message: string;
+}
+
+export interface SimpleEventDTO {
+  id: number,
+  avatarUrl?: string,
+  username: string,
+  scheduleTime: string,
+  mood?: string,
+  meetPoint: string,
+  restaurantName: string,
+  message?: string,
+}
