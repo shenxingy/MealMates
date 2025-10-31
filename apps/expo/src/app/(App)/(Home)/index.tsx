302c2368774efd3b51c7fb78dbaa4d34ce5d6895
chitose <103087652@qq.com>
2025-10-31 17:59:03 -0400

    feat: add EventDetails layout and page; implement navigation to event details from HomePage

--- apps/expo/src/app/(App)/(Home)/index.tsx
+++ apps/expo/src/app/(App)/(Home)/index.tsx
@@ -1,20 +1,28 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 import { useQuery } from "@tanstack/react-query";
 
 import { fetchSimpleEventList } from "~/utils/api";
 import AnimatedPageFrame from "../../../../components/frame/AnimatedPageFrame";
 import EventView from "../../../../components/homepage/EventView";
+import { useRouter } from "expo-router";
 
 export default function HomePage() {
   // Gradient color
   const baseColor = "255,120,0";
   const header = "MealMate";
+  const router = useRouter();
 
   // useQuery to fetch simple event list
   const { data, isLoading, error } = useQuery({
     queryKey: ["simpleEventList"],
     queryFn: fetchSimpleEventList,
   });
+
+  const handleEventPress = (eventId: number) => {
+    console.log("Event", eventId, "Pressed!");
+    // Navigate to event details page
+    router.push(`/event/${eventId}`);
+  }
 
   if (isLoading) {
     return (
@@ -35,8 +43,13 @@ export default function HomePage() {
 
   if (error) {
     return (
-      <AnimatedPageFrame baseColor={baseColor} headerTitle={header}>
-        <Text>Error loading data</Text>
+      <AnimatedPageFrame baseColor={baseColor} headerTitle={header} scrollEnabled={false}>
+        <View style={styles.emptySpace} />
+        {Array.from({ length: 3 }).map((_, i) => (
+          <View style={{ marginBottom: 20 }} key={i}>
+            <EventView isLoading={true} />
+          </View>
+        ))}
       </AnimatedPageFrame>
     );
   }
@@ -45,11 +58,11 @@ export default function HomePage() {
     <AnimatedPageFrame baseColor={baseColor} headerTitle={header}>
       <View style={styles.emptySpace} />
 
-      {data?.map((event, i) => (
-        <View key={i} style={{ marginBottom: 20 }}>
+      {data?.map((event, _) => (
+        <View key={event.id} style={{ marginBottom: 20 }}>
           <Pressable
             onPress={() => {
-              console.log(`Event ${i} Pressed!`);
+              handleEventPress(event.id);
             }}
           >
             <EventView
