eba6ee8b895a091927fde14df5745deb0f09ffc8
Xingyu(Alex) Shen <shen13515251251@gmail.com>
2025-11-03 11:39:23 -0500

    fix: disable new architecture in Expo app configuration

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -12,7 +12,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   updates: {
     fallbackToCacheTimeout: 0,
   },
-  newArchEnabled: true,
+  newArchEnabled: false,
   assetBundlePatterns: ["**/*"],
   ios: {
     bundleIdentifier: "com.mealmates.app",
