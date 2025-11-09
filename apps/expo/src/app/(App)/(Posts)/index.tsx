8c1f4f918b97735bbd0fdbd4b78ac467f2e001bc
Yongqi Shi <yongqi.shi@duke.com>
2025-11-09 17:56:41 -0500

    posts home page

--- apps/expo/src/app/(App)/(Posts)/index.tsx
+++ apps/expo/src/app/(App)/(Posts)/index.tsx
@@ -1,32 +1,31 @@
-import { FlatList } from "react-native";
+import { StyleSheet } from "react-native";
 import AnimatedPageFrame from "../../../../components/frame/AnimatedPageFrame";
-import PostItem from "../../../../components/postpage/PostItem";
+import PostItem, { PostProps } from "../../../../components/postpage/PostItem";
+import PostList from "../../../../components/postpage/PostList";
 
 const DATA: PostProps[] = [
   {
     title: 'Today\'s Skillet!',
     image: 'https://www.cookingclassy.com/wp-content/uploads/2022/07/grilled-steak-15.jpg',
-    user: '',
+    user: 'user1',
     likes: 10,
     liked: true
   },
   {
-    title: 'Wonderful!',
-    image: 'https://tse2.mm.bing.net/th/id/OIP.wdhC1w1RKrP1fcEJaUVcswHaLH?rs=1&pid=ImgDetMain&o=7&rm=3',
-    user: '',
-    likes: 100,
+    title: 'vege curry',
+    image: 'https://currykits.com/wp-content/uploads/2017/08/Indian-vegetable-curry.jpg',
+    user: 'user3',
+    likes: 0,
     liked: false
   },
+  {
+    title: 'Wonderful!',
+    image: 'https://tse2.mm.bing.net/th/id/OIP.wdhC1w1RKrP1fcEJaUVcswHaLH?rs=1&pid=ImgDetMain&o=7&rm=3',
+    user: 'user2',
+    likes: 100,
+    liked: false
+  }
 ];
-
-type PostProps = {
-  title: string;
-  image: string;
-  user: string;
-  likes: number;
-  liked: boolean;
-}
-
 
 export default function PostPage() {
   const header = "Posts";
@@ -47,13 +46,24 @@ const renderPost = (post : any) => {
   return (
     <>
       <AnimatedPageFrame baseColor={baseColor} headerTitle={header}>
-        <FlatList
+        <PostList
           data={DATA}
-          renderItem={renderPost}
           numColumns={2}
-          horizontal={false}
         />
       </AnimatedPageFrame>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+  posts: {
+    fontSize: 30,
+    marginTop: 30
+  },
+  bg: {
+    backgroundColor: '#ff0'
+  }
+})
