6fefd4a141600ce67fb2fafa3ecd18d1df6f4e19
Yongqi Shi <yongqi.shi@duke.com>
2025-11-21 13:16:56 -0500

    fixing image picker

--- apps/expo/src/app/(App)/(Posts)/index.tsx
+++ apps/expo/src/app/(App)/(Posts)/index.tsx
@@ -1,8 +1,15 @@
 import AnimatedPageFrame from "../../../../components/frame/AnimatedPageFrame";
+import * as ImagePicker from 'expo-image-picker';
 
 export default function PostPage() {
   const header = "Posts";
   const baseColor = "255,178,0";
+  const pick = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      quality: 1,
+    });
+  }
   return (
     <>
       <AnimatedPageFrame baseColor={baseColor} headerTitle={header}>
