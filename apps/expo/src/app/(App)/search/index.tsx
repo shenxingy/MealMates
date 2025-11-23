375d6a5e343401e798217b755ffdd3acd8cbbb44
Xingyu(Alex) Shen <shen13515251251@gmail.com>
2025-11-23 10:26:04 -0500

    remove login alert, enabled search from the database (ios working)

--- apps/expo/src/app/(App)/search/index.tsx
+++ apps/expo/src/app/(App)/search/index.tsx
@@ -1,26 +1,32 @@
-import { Text } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 
-
-
+import useDebounce from "~/hooks/useDebounce";
 import AnimatedPageFrame from "../../../../components/frame/AnimatedPageFrame";
+import SearchResultsList from "./SearchResultsList";
 
+import useDebounce from "~/hooks/useDebounce";
+import AnimatedPageFrame from "../../../../components/frame/AnimatedPageFrame";
+import SearchResultsList from "./SearchResultsList";
 
 export default function SearchPage() {
   const baseColor = "255,120,0";
   const header = "Search";
-  const {query}: {query: string} = useLocalSearchParams()
+  const { query } = useLocalSearchParams();
+  const searchQuery =
+    typeof query === "string"
+      ? query
+      : Array.isArray(query)
+        ? (query[0] ?? "")
+        : "";
+  const debouncedQuery = useDebounce(searchQuery);
+
   return (
-    <>
-      <AnimatedPageFrame
-        baseColor={baseColor}
-        headerTitle={header}
-        scrollEnabled={false}
-      >
-        <>
-          <Text>{query}</Text>
-        </>
-      </AnimatedPageFrame>
-    </>
+    <AnimatedPageFrame
+      baseColor={baseColor}
+      headerTitle={header}
+      scrollEnabled={false}
+    >
+      <SearchResultsList query={searchQuery} debouncedQuery={debouncedQuery} />
+    </AnimatedPageFrame>
   );
 }
