9ce778dd33e9a69455d7d8cbb30fe5b0386f2af3
Yongqi Shi <yongqi.shi@duke.com>
2025-11-22 21:05:24 -0500

    feat: post schema

---
+++ packages/db/src/post-schema.ts
@@ -0,0 +1,61 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod/v4";
+import { user } from "./auth-schema";
+
+// export const Post = pgTable("post", (t) => ({
+//   id: t.uuid().notNull().primaryKey().defaultRandom(),
+//   title: t.varchar({ length: 256 }).notNull(),
+//   content: t.text().notNull(),
+//   createdAt: t.timestamp().defaultNow().notNull(),
+//   updatedAt: t
+//     .timestamp({ mode: "date", withTimezone: true })
+//     .$onUpdateFn(() => sql`now()`),
+// }));
+
+export const post = pgTable("post", {
+  id: uuid("id").primaryKey().notNull(),
+  userId: text("user_id")
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+  title: text("title"),
+  content: text("content"),
+  image: text("image").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const comment = pgTable("comment", {
+  id: uuid("id").primaryKey().notNull(),
+  postId: uuid("post_id")
+      .notNull()
+      .references(() => post.id, { onDelete: "cascade" }),
+  content: text("content"),
+  image: text("image")
+});
+
+export const postLike = pgTable("post_like", {
+  postId: uuid("post_id")
+      .notNull()
+      .references(() => post.id, { onDelete: "cascade" }),
+  userId: text("user_id")
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+});
+
+export const commentLike = pgTable("comment_like", {
+  commentId: uuid("comment_id")
+      .notNull()
+      .references(() => comment.id, { onDelete: "cascade" }),
+  userId: text("user_id")
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+});
+
+// export const CreatePostSchema = createInsertSchema(post, {
+//   title: z.string().max(256),
+//   content: z.string().max(256),
+// }).omit({
+//   id: true,
+//   createdAt: true,
+//   updatedAt: true,
+// });
