0d0a89beb493f515b6bf67017b39c135d7594511
Matty-7 <jh730@duke.edu>
2025-11-23 13:54:20 -0500

    refactor: remove meet point details from mock event data to streamline event representation

--- apps/nextjs/src/app/mock/mock.ts
+++ apps/nextjs/src/app/mock/mock.ts
@@ -8,7 +8,6 @@ export const simpleEventMockData: SimpleEventDTO[] = [
     username: "Nagasaki Soyo",
     scheduleTime: "12:20",
     mood: "🤓",
-    meetPoint: "Tsuki no mori Girls' School",
     restaurantName: "RING",
     message:
       "Hello! Let's have lunch together. Let me know if you're interested. Could be fun!",
@@ -20,7 +19,6 @@ export const simpleEventMockData: SimpleEventDTO[] = [
     username: "Tanaka Hiroshi",
     scheduleTime: "12:30",
     mood: "😎",
-    meetPoint: "Central Park Entrance",
     restaurantName: "Sushi World",
     message: "Looking forward to meeting new people for lunch!",
   },
@@ -30,7 +28,6 @@ export const simpleEventMockData: SimpleEventDTO[] = [
       "https://rcucryvgjbthzoirnzam.supabase.co/storage/v1/object/public/Avatar/avatar_user_3.jpeg",
     username: "Yamamoto Aiko",
     scheduleTime: "12:40",
-    meetPoint: "City Library",
     restaurantName: "Pasta Palace",
     message: "Anyone up for some Italian cuisine?",
   },
@@ -39,7 +36,6 @@ export const simpleEventMockData: SimpleEventDTO[] = [
     username: "Kobayashi Ken",
     scheduleTime: "12:50",
     mood: "🍣",
-    meetPoint: "Downtown Cafe",
     restaurantName: "Sushi Express",
     message: "Sushi lovers, let's gather for lunch!",
   },
@@ -48,7 +44,6 @@ export const simpleEventMockData: SimpleEventDTO[] = [
     username: "Saito Mei",
     scheduleTime: "13:10",
     mood: "🥗",
-    meetPoint: "University Campus",
     restaurantName: "Green Bites",
     message: "Healthy lunch meetup, anyone?",
   },
@@ -58,31 +53,26 @@ export const detailedEventMockData: DetailedEventDTO[] = [
   {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     ...simpleEventMockData[0]!,
-    meetPointCoordinates: { latitude: 36.00162, longitude: -78.93963 },
     restaurantCoordinates: { latitude: 36.01126, longitude: -78.92182 },
   },
   {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     ...simpleEventMockData[1]!,
-    meetPointCoordinates: { latitude: 36.00162, longitude: -78.93963 },
     restaurantCoordinates: { latitude: 36.01126, longitude: -78.88182 },
   },
   {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     ...simpleEventMockData[2]!,
-    meetPointCoordinates: { latitude: 36.00162, longitude: -78.93963 },
     restaurantCoordinates: { latitude: 36.00162, longitude: -78.93363 },
   },
   {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     ...simpleEventMockData[3]!,
-    meetPointCoordinates: { latitude: 36.00162, longitude: -78.93963 },
     restaurantCoordinates: { latitude: 36.00162, longitude: -78.93853 },
   },
   {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     ...simpleEventMockData[4]!,
-    meetPointCoordinates: { latitude: 40.7128, longitude: -74.006 },
     restaurantCoordinates: { latitude: 40.713, longitude: -74.0062 },
   },
 ];
